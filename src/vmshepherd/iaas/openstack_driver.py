82625ac621c9e2e5ed4896d88b480ca789584419
Krzysztof Warunek <krzysztof@warunek.net>
2018-02-19 07:51:51 +0100

    import from stash

--- src/vmshepherd/iaas/openstack_driver.py
+++ src/vmshepherd/iaas/openstack_driver.py
@@ -16,7 +16,7 @@ from novaclient.exceptions import ClientException, NotFound, MethodNotAllowed, N
 class OpenStackDriver(AbstractIaasDriver):
 
     def __init__(self, *args, **config):
-        self.config = kwargs
+        self.config = config
 
     def openstack_exception(func):
         '''
