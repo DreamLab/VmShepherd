06576883f5c849698ff8c28a1ea8e9d80d6cded1
Mostowiec Dominik <Dominik.Mostowiec@dreamlab.pl>
2018-02-21 14:52:17 +0100

    import from stash

--- src/vmshepherd/utils.py
+++ src/vmshepherd/utils.py
@@ -62,7 +62,7 @@ class PrefixFilter(logging.Filter):
         self._prefix = prefix
 
     def filter(self, record):
-        if 'preset' in record.__dict__:
+        if hasattr(record, 'preset'):
             record.msg = '[%s][%s][%s]: %s' % (self._prefix, record.preset, record.module, record.msg)
         else:
             record.msg = '[%s][%s]: %s' % (self._prefix, record.module, record.msg)
