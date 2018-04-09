1cbc127a5d6872881af79d4d0edf72cc6c1b3d90
Krzysztof Warunek <krzysztof@warunek.net>
2018-04-09 10:18:58 +0200

    import from stash

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(name):
 
 setup(
     name="vmshepherd",
-    version="0.7.3",
+    version="0.7.4",
     author='Dreamlab - PaaS KRK',
     author_email='paas-support@dreamlab.pl',
     url='https://github.com/Dreamlab/vmshepherd',
